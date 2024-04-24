b74419be2ab90df47fa20690057edc5d84d8cad1
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-04-24 11:25:02 +0100

    Add nodemon for development server restart on file change
    This commit adds the "nodemon" package to automatically restart the development server when a file changes. The "dev" script in package.json has been updated to use nodemon with src

--- src/server.js
+++ src/server.js
@@ -1,11 +1,34 @@
 import express from 'express';
 
+const articleInfo= [
+  {
+    name: 'learn-react',
+    upvote: 0,
+  },
+  {
+    name: 'learn-node',
+    upvote: 0,
+  },
+  {
+    name: 'mongodb',
+    upvote: 0,
+  }
+]
+
 const app = express();
 app.use(express.json())
 
-app.post('/hello', (req, res) => {
-  res.send(`Hello ${req.body.name}!`);
-});
+app.put('/api/articles/:name/upvote', (req, res)=> {
+  const name = req.params.name;
+  const article = articleInfo.find(artc => artc.name === name);
+  if (article) {
+    article.upvote += 1;
+    res.send(`This ${name} article now has ${article.upvote} upvotes!!!`);
+  } else {
+    res.send("This article doesn\'t exist");
+  }
+  
+})
 
 app.listen(8000, () => {
   console.log('Server is listening on port 8000!');
