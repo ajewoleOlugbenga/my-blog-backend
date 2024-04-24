12faa6d6a5ca11a0abd1aa9da7ea62f01b214e4a
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-04-24 12:01:53 +0100

    "Updated server.js with put and post requests for upvoting and commenting on articles, added else statements for handling non-existent articles"
    This commit updates the server.js file by adding a put request to handle upv

--- src/server.js
+++ src/server.js
@@ -1,35 +1,50 @@
-import express from 'express';
+import express from "express";
 
-const articleInfo= [
+const articleInfo = [
   {
-    name: 'learn-react',
+    name: "learn-react",
     upvote: 0,
+    Comment: [],
   },
   {
-    name: 'learn-node',
+    name: "learn-node",
     upvote: 0,
+    Comment: [],
   },
   {
-    name: 'mongodb',
+    name: "mongodb",
     upvote: 0,
-  }
-]
+    Comment: [],
+  },
+];
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.put('/api/articles/:name/upvote', (req, res)=> {
+app.put("/api/articles/:name/upvote", (req, res) => {
   const name = req.params.name;
-  const article = articleInfo.find(artc => artc.name === name);
+  const article = articleInfo.find((artc) => artc.name === name);
   if (article) {
     article.upvote += 1;
     res.send(`This ${name} article now has ${article.upvote} upvotes!!!`);
   } else {
+    res.send("This article doesn't exist");
+  }
+});
+
+app.post("/api/articles/:name/Comments", (req, res) => {
+  const name = req.params.name;
+  const { postedBy, text } = req.body;
+  //can also be writting as const {name}= req.params;
+  const article = articleInfo.find((a) => a.name === name);
+  if (article) {
+    article.Comment.push({ postedBy, text });
+    res.send(`"${postedBy}" wrote "${text}"`);
+  } else {
     res.send("This article doesn\'t exist");
   }
-  
-})
+});
 
 app.listen(8000, () => {
-  console.log('Server is listening on port 8000!');
+  console.log("Server is listening on port 8000!");
 });
